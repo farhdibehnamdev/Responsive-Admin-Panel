4b73f86bf53e624fb06caa48c3c0b8bb6fb1065e
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-10-21 19:00:16 +0330

    ✨ feat:Implemented sidebar toggling.

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 "use strict";
 const body = document.querySelector("body");
 const toggleMode = document.querySelector(".toggle-mode");
-
 const switchElement = document.querySelector(".toggle-mode .switch");
-let darkMode = false;
+const sidebarToggle = document.querySelector(".sidebarToggle");
+const sidebar = document.querySelector("nav");
 const darkModeHandler = function () {
   body.classList.toggle("dark");
   switchElement.classList.toggle("darked");
 };
 
+const sidebarHandler = function () {
+  sidebar.classList.toggle("close");
+};
+
 toggleMode.addEventListener("click", () => darkModeHandler());
+sidebarToggle.addEventListener("click", () => sidebarHandler());
